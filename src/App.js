1547c6ec991b65dc64babffab4b4be19ea1e9760
jannis.grebien <jannisgrebien@web.de>
2020-10-12 17:19:36 +0200

    Add a scrollable Component for card elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css';
 
 
@@ -40,7 +41,9 @@ class App extends React.Component {
                 <div className='tc'>
                 <h1 className='f2'>RoboBuddies</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
     
         );
